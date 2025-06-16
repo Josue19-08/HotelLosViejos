8a46b982aa74854dc2607d50a8e41b21df56f0f7
Daniel Barrantes <danielbarrantesq@gmail.com>
2025-06-15 21:54:20 -0600

    Commit

--- Backend/src/main/java/com/hotelLosViejos/HotelLosViejos/Dominio/Reserva.java
+++ Backend/src/main/java/com/hotelLosViejos/HotelLosViejos/Dominio/Reserva.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Reserva {
+public class  Reserva {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
