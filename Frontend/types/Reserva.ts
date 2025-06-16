1ff010cf8599cfd24febb9bcf052a8c75c3cc929
Daniel Barrantes <danielbarrantesq@gmail.com>
2025-06-15 22:50:03 -0600

    Se corrige problema de listado de reservaciones

--- Frontend/types/Reserva.ts
+++ Frontend/types/Reserva.ts
@@ -1,21 +1,20 @@
 import type { ClienteBase } from './Cliente';
 import type { HabitacionBase } from './Habitacion';
-
 export interface ReservaPayload {
-  nombre: string
-  apellidos: string
-  correo: string
-  numeroTarjeta: string
-  habitacionId: number
-  fechaLlegada: string 
-  fechaSalida: string
-}
+    nombre: string
+    apellidos: string
+    correo: string
+    numeroTarjeta: string
+    habitacionId: number
+    fechaLlegada: string 
+    fechaSalida: string
+  }
 
 export interface ReservaLectura {
-  id: number
-  numeroReserva: string
-  fechaLlegada: string
-  fechaSalida: string
-  cliente: ClienteBase
+  id: number,
+  numeroReserva: string,
+  fechaLlegada: string,
+  fechaSalida: string,
+  cliente: ClienteBase,
   habitacion: HabitacionBase
 }
